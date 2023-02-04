9e88a9e6122743976f33332e6d9e262fc10b42cb
Daniel Ramirez <ramirezdani016@hotmail.com>
2023-02-04 16:05:25 -0300

    commit

--- script.js
+++ script.js
@@ -39,7 +39,13 @@ const productsList = [
   ),
 ];
 
-const carrito = [];
+let carrito = [];
+
+if (localStorage.getItem("carrito")) {
+  let carritoEnJSON = localStorage.getItem("carrito");
+  carrito = JSON.parse(carritoEnJSON);
+  renderizarCarrito();
+}
 
 let filtros = document.getElementById("buscar");
 filtros.onclick = function () {
@@ -52,12 +58,16 @@ filtros.onclick = function () {
       p.marca.toLowerCase().includes(buscador.value.toLowerCase())
   );
 
-  console.log(productosFiltrados);
+  document.getElementById("productos").innerHTML = "";
+
+  productosFiltrados.forEach((x) => renderizarProductos(x));
 };
 
 let divProductos = document.getElementById("productos");
+productsList.forEach((x) => renderizarProductos(x));
 
-productsList.forEach((x) => {
+function renderizarProductos(x) {
+  let divProductos = document.getElementById("productos");
   let divProducto = document.createElement("div");
   divProducto.className = "col-4 my-2";
 
@@ -82,9 +92,72 @@ productsList.forEach((x) => {
   let cardButton = document.createElement("button");
   cardButton.type = "button";
   cardButton.className = "btn btn-primary";
-  cardButton.textContent = "comprar";
+  cardButton.textContent = "Comprar";
   cardButton.id = x.id;
+  cardButton.onclick = agregarCarrito;
   cardBody.append(cardButton);
 
   divProductos.append(divProducto);
-});
+}
+
+function agregarCarrito(e) {
+  let id = e.target.id;
+  let producto = productsList.find((p) => p.id == id);
+  let productoEnCarrito = carrito.find((p) => p.id == id);
+
+  if (productoEnCarrito) {
+    let i = carrito.findIndex((x) => x.id == id);
+    carrito[i].unidades++;
+    carrito[i].subtotal = carrito[i].precio * carrito[i].unidades;
+  } else {
+    producto.unidades = 1;
+    producto.subtotal = producto.precio;
+    carrito.push(producto);
+  }
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+
+  renderizarCarrito();
+}
+
+function renderizarCarrito() {
+  let divCarrito = document.getElementById("carrito");
+  divCarrito.innerHTML = "";
+
+  carrito.forEach((x) => {
+    let divProductoEnCarrito = document.createElement("div");
+    divProductoEnCarrito.className = "col-12 my-2";
+
+    let divCard = document.createElement("div");
+    divCard.className = "card";
+    divProductoEnCarrito.append(divCard);
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    divCard.append(cardBody);
+
+    let cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.innerText = x.nombre;
+    cardBody.append(cardTitle);
+
+    let cardUnidades = document.createElement("p");
+    cardUnidades.className = "card-text";
+    cardUnidades.innerHTML = `Unidades: ${x.unidades}`;
+    cardBody.append(cardUnidades);
+
+    let cardPrecio = document.createElement("p");
+    cardPrecio.className = "card-text";
+    cardPrecio.innerHTML = `SUBTOTAL: $${x.subtotal}`;
+    cardBody.append(cardPrecio);
+
+    divCarrito.append(divProductoEnCarrito);
+  });
+}
+
+let finalizarCompra = document.getElementById("finalizar");
+finalizarCompra.onclick = function () {
+  localStorage.removeItem("carrito");
+  carrito = [];
+  renderizarCarrito();
+};
